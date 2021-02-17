a5e089a784a2e911ab432c12c21af406ba121fa2
Shreya Nayak <shryanayak97@gmail.com>
2021-02-17 11:14:20 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//import Game from './src/components/Game';
+import Game from './Game';
+
+class App extends React.Component {
+  state = {
+    gameId: 1,
+  };
+  resetGame = () => {
+    this.setState((prevState) => {
+      return { gameId: prevState.gameId + 1};
+    });
+  };
+  render() {
+    return (
+      <Game 
+        key={this.state.gameId} 
+        onPlayAgain={this.resetGame}
+        randomNumberCount={6} 
+        initialSeconds={10}
+        />
+    );
+  }
+}
+
+
+export default App;
+
+
